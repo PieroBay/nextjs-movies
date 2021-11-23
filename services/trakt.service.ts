703a78be1ccf17c60f83c6e5b4214f3dbe7353ab
Pierobay <pierobay05@gmail.com>
2021-11-23 16:03:12 +0100

    feat(login): add trakt uri

--- services/trakt.service.ts
+++ services/trakt.service.ts
@@ -8,24 +8,24 @@ export class TraktService {
     constructor(private axios: Axios) {}
 
     public genOauthLink(): string {
-        return `https://trakt.tv/oauth/authorize?response_type=code&client_id=${environment.CLIENT_ID}&redirect_uri=${environment.REDIRECT_URI}`;
+        return `${environment.TRAKT_URI}oauth/authorize?response_type=code&client_id=${environment.CLIENT_ID}&redirect_uri=${environment.REDIRECT_URI}`;
     }
 
     public getAccessToken(code: string): Promise<TraktAccessInterface> {
         const model = new TraktPostTokenModel();
         model.code = code;
-        return this.axios.post('https://api.trakt.tv/oauth/token', model.toAccessDto());
+        return this.axios.post(`${environment.TRAKT_URI}ooauth/token`, model.toAccessDto());
     }
 
     public refreshAccessToken(refreshToken: string): Promise<TraktAccessInterface> {
         const model = new TraktPostTokenModel();
         model.refreshToken = refreshToken;
-        return this.axios.post('https://api.trakt.tv/oauth/token', model.toRefreshTokenDto());
+        return this.axios.post(`${environment.TRAKT_URI}ooauth/token`, model.toRefreshTokenDto());
     }
 
     public revokeToken(token: string): Promise<TraktAccessInterface> {
         const model = new TraktPostTokenModel();
         model.token = token;
-        return this.axios.post('https://api.trakt.tv/oauth/revoke', model.toRevokeTokenDto());
+        return this.axios.post(`${environment.TRAKT_URI}ooauth/revoke`, model.toRevokeTokenDto());
     }
 }
