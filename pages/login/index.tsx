172148dc0b10d8ee550a4c74e1de104020c4a375
l.halleux <l.halleux@afelio.be>
2021-11-23 09:14:57 +0100

    Creation of route

---
+++ pages/login/index.tsx
@@ -0,0 +1,10 @@
+import type {NextPage} from 'next'
+import {Typography} from "@mui/material";
+
+const Login: NextPage = () => {
+    return <div>
+        <Typography variant="h1">Login work!</Typography>
+    </div>
+}
+
+export default Login
---
+++ pages/search/index.tsx
@@ -0,0 +1,10 @@
+import type {NextPage} from 'next'
+import {Typography} from "@mui/material";
+
+const Search: NextPage = () => {
+    return <div>
+        <Typography variant="h1">Search work!</Typography>
+    </div>
+}
+
+export default Search
